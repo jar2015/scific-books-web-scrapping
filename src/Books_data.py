3cbcd2d28017927560b785b448f33920d13a98cb
jar2015 <57324369+jar2015@users.noreply.github.com>
2019-11-05 14:03:41 -0600

    Update Books_data.py

--- src/Books_data.py
+++ src/Books_data.py
@@ -27,27 +27,24 @@ for page in pages:
     
     #LOOP TO SEARCH IN THE CONTAINER THE VARIABLES NEEDED
     for container in books_container:
-    
         #SEARCH AND APPEND THE BOOK AUTHOR'S NAME
         name = container.find('a', class_ = 'item-author').text
         author_name.append(name)
-        
         #SEARCH AND APPEND THE BOOK NAME
         book = container.find('a', class_ = 'word-break').text
         book_name.append(book)
-        
         #SEARCH AND APPEND BOOK DETAILS
         details = container.find('ul', class_ = 'item-details').text
         details_desc.append(details)
 
-#CREATE DATAFRAME WITH FIELDS CREATED BEFORE
-df_books = pd.DataFrame({'author': author_name,
-'book': book_name,
+#CREATE DATAFRAME WITH FIELDS CREATED BEOFRE
+df_books = pd.DataFrame({'Author Name': author_name,
+'Book Name': book_name,
 'details': details_desc
 })
 
 #SPLIT DETAILS FIELD IN MORE COLUMNS
-new = df_books['details'].str.split('\n', n=5, expand= True)
+new = df_books['details'].str.split('\n', n=6, expand= True)
 df_books["Language"]= new[1] 
 df_books["Words"]= new[2]
 df_books["Age"]= new[3]
@@ -57,6 +54,9 @@ df_books["Favorite"]= new[5]
 #DELETE DETAILS FIELD BECAUSE IT WAS SPLITTED 
 df_books.drop(columns =['details'], inplace = True) 
 
-#REMOVE SPECIFIC STRING IN COLUMNS
+#REMOVE WORDS IN COLUMNS
 df_books["Words"] = df_books["Words"].str.replace(' Words','')
 df_books["Age"] = df_books["Age"].str.replace('Ages ','')
+
+#CREATED A CSV FILE USING DATAFRAME CREATED
+df_books.to_csv(r'C:\Users\rodriguezjos\OneDrive - VMware, Inc\VMwareCorp\Desktop\Master Data Sciences\3- II Semestre 2019\Tipologia y cliclo de vida de los datos\PRAC 1\Books_dataset.csv', index=False)
